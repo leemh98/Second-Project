6141b4b4ead43b39b308a13a4b4f93648a2ebb4f
Lmh <1004alsgud@gmail.com>
2024-05-23 21:34:57 +0900

    0523 books and category

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ app.listen(process.env.PORT); // 유의미한 포트 번호 지정하는 것이 
 
 const userRouter = require('./routes/users');
 const bookRouter = require('./routes/books');
+const categoryRouter = require('./routes/category');
 const likeRouter = require('./routes/likes');
 const cartRouter = require('./routes/carts');
 const orderRouter = require('./routes/orders');
 
 app.use("/users", userRouter);
 app.use("/books", bookRouter);
+app.use("/category", categoryRouter);
 app.use("/likes", likeRouter);
 app.use("/carts", cartRouter);
 app.use("/orders", orderRouter);
